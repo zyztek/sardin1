f10a31b642b0bdb888ac3faa369affd140f391b5
David Enrique Zayas Piña <12541299+zyztek@users.noreply.github.com>
2026-03-14 20:18:45 +0000

    Auto-commit: 1 modified file

--- dashboard/src/app/layout.tsx
+++ dashboard/src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang=\"es-MX\">
+    <html lang="es-MX">
       <body className={inter.className}>{children}</body>
     </html>
   )
